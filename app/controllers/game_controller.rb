5b2109f35eeca9c4e30236cb5fc9ffcedf6575ca
Ayesha-42 <62644991+Ayesha-42@users.noreply.github.com>
2022-07-13 14:37:11 +1000

    game logic added to check the user input and update instance variables accordingly

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -9,4 +9,17 @@ class GameController < ApplicationController
     @hidden_word = @word_to_guess.chars.map { '_' }
   end
 
+  def word_guess
+    @user_guess = params[:guess] #.chomp.downcase
+  
+    if @word_to_guess.include? @user_guess
+      @hidden_word = @hidden_word.filter_map.with_index do |letter, index|
+        @user_guess == @word_to_guess[index] ? @user_guess : letter
+      end
+    else
+      @lives -= 1
+    end
+  
+    render('v2')
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   root "game#start"
   get "/game", to: "game#start"
 
+  get "/game/word_guess", to: "game#word_guess"
+  post '/word_guess' => 'game#word_guess'
+
 end
