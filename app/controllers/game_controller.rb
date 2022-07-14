6f939f766fd11b53ae075581155b4dc54e0c9bf9
Ayesha-42 <62644991+Ayesha-42@users.noreply.github.com>
2022-07-14 11:57:20 +1000

    all rubocop offenses addressed.

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -1,19 +1,18 @@
 class GameController < ApplicationController
-
   skip_before_action :verify_authenticity_token
-  
+
   def initialize
     super
-    @lives = 9
-    @words = %w[programming fun internship ruby rails coding design]
-  
-    @word_to_guess = @words.sample
-    @hidden_word = @word_to_guess.chars.map { '_' }
+    @lives ||= 9
+    @words ||= %w[programming fun internship ruby rails coding design]
+
+    @word_to_guess ||= @words.sample
+    @hidden_word ||= @word_to_guess.chars.map { "_" }
   end
 
   def word_guess
-    @user_guess = params[:guess] #.chomp.downcase
-  
+    @user_guess = params[:guess] # .chomp.downcase
+
     if @word_to_guess.include? @user_guess
       @hidden_word = @hidden_word.filter_map.with_index do |letter, index|
         @user_guess == @word_to_guess[index] ? @user_guess : letter
@@ -21,17 +20,15 @@ class GameController < ApplicationController
     else
       @lives -= 1
     end
-    if @hidden_word.join('') == @word_to_guess
+    if @hidden_word.join("") == @word_to_guess
       @message = "You won the game!"
-      render('result')
+      render("result")
     end
-    if @lives<1
+    if @lives < 1
       @message = "Unfortunately, you've run out of chances to guess, better luck next time!"
-      render('result')
+      render("result")
     end
 
-    render('word_guess')
+    render("word_guess")
   end
-
-
 end
