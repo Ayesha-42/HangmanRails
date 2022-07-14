e1b91241416450df534d4d77f7c6c7eac73776b3
Ayesha-42 <62644991+Ayesha-42@users.noreply.github.com>
2022-07-14 15:53:01 +1000

    minor rubocop offense fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  root 'game#start'
-  get '/game', to: 'game#start'
+  root "game#start"
+  map.root :controller => "start"
+  get "/game", to: "game#start"
 
-  get '/game/word_guess', to: 'game#word_guess'
+  get "/game/word_guess", to: "game#word_guess"
   post '/word_guess' => 'game#word_guess'
 
-  get '/game/result', to: 'game#result'
+  get "/game/result", to: "game#result"
 end
