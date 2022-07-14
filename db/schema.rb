5174a745d7827010cd072ecd8a1026ca41a3b44b
Ayesha-42 <62644991+Ayesha-42@users.noreply.github.com>
2022-07-14 16:20:35 +1000

    deleted previous games table

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,5 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_12_023616) do
-  create_table "games", force: :cascade do |t|
-    t.string "title"
-    t.text "body"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
+ActiveRecord::Schema[7.0].define(version: 2022_07_14_061618) do
 end
