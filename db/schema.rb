b4caf7b440192338fbbc58d240f39ef17c33bb7e
Ayesha-42 <62644991+Ayesha-42@users.noreply.github.com>
2022-07-15 13:01:19 +1000

    creating a record with the word_to_guess in the table GameSessions

--- db/schema.rb
+++ db/schema.rb
@@ -10,5 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_14_061618) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_14_062422) do
+  create_table "game_sessions", force: :cascade do |t|
+    t.string "word_to_guess"
+    t.string "guesses"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
 end
