bd215db456c7abd8b95fc551ac0cc421e74f65fe
Ayesha-42 <62644991+Ayesha-42@users.noreply.github.com>
2022-07-21 18:38:06 +1000

    test file structure improved.

--- spec/models/game_session_spec.rb
+++ spec/models/game_session_spec.rb
@@ -1,22 +1,28 @@
 require 'rails_helper'
+require 'support/custom_matchers'
+
 
 RSpec.describe GameSession, type: :model do
-  subject { GameSession.new }
-  before { subject.save }
 
-  it 'word_to_guess should be assigned' do
-    expect(subject.word_to_guess).to_not eq(nil)
+  context 'class constants' do
+    it 'should have an array list to select from' do
+      defined? GameSession::WORDS
+    end
   end
 
-  it 'should have an array list to select from' do
-    defined? GameSession::WORDS
-  end
+  context '.create' do
+    subject { GameSession.create }
 
-  it 'should select word_to_guess from the words array' do
-    expect(subject.word_to_guess).to be_included_in(GameSession::WORDS)
-  end
+    it 'word_to_guess should be assigned' do
+      expect(subject.word_to_guess).to_not eq(nil)
+    end
 
-  it 'should not have any user guesses to check' do
-    expect(subject.guesses).to eq(nil)
+    it 'should select word_to_guess from the words array' do
+      expect(subject.word_to_guess).to be_included_in(GameSession::WORDS)
+    end
+
+    it 'should not have any user guesses to check' do
+      expect(subject.guesses).to eq(nil)
+    end
   end
 end
