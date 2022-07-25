cb1559c582e3cc5c08b42e45b641b2e373834448
Ayesha-42 <62644991+Ayesha-42@users.noreply.github.com>
2022-07-25 15:15:04 +1000

    tests for the `lives remaining` method in GameSession  model describe block restructure as well

--- spec/models/game_session_spec.rb
+++ spec/models/game_session_spec.rb
@@ -1,28 +1,45 @@
 require 'rails_helper'
 require 'support/custom_matchers'
 
+RSpec.describe 'GameSession model class constants' do
+  it 'should have an array list to select from' do
+    defined? GameSession::WORDS
+  end
+end
 
-RSpec.describe GameSession, type: :model do
+RSpec.describe 'GameSession.create' do
+  subject { GameSession.create }
+  it 'word_to_guess should be assigned' do
+    expect(subject.word_to_guess).to_not eq(nil)
+  end
+  it 'should select word_to_guess from the words array' do
+    expect(subject.word_to_guess).to be_included_in(GameSession::WORDS)
+  end
+  it 'should not have any user guesses to check' do
+    expect(subject.guesses).to eq(nil)
+  end
+end
 
-  context 'class constants' do
-    it 'should have an array list to select from' do
-      defined? GameSession::WORDS
+RSpec.describe 'GameSession methods' do
+  let(:test_record) { GameSession.new(word_to_guess: 'test') }
+
+  context '#lives_remaining' do
+    it 'correct initial lives_remaining' do
+      test_record.guesses = ''
+      expect(test_record.lives_remaining).to eq(9)
+    end
+    it 'correctly decuting lives with incorrect guess' do
+      test_record.guesses = 'a'
+      expect(test_record.lives_remaining).to eq(8)
+    end
+    it 'correctly decuting lives with incorrect guesses' do
+      test_record.guesses = 'aa'
+      expect(test_record.lives_remaining).to eq(7)
+    end
+    it 'correctly dattributing lives' do
+      test_record.guesses = 'aat'
+      expect(test_record.lives_remaining).to eq(7)
     end
   end
 
-  context '.create' do
-    subject { GameSession.create }
-
-    it 'word_to_guess should be assigned' do
-      expect(subject.word_to_guess).to_not eq(nil)
-    end
-
-    it 'should select word_to_guess from the words array' do
-      expect(subject.word_to_guess).to be_included_in(GameSession::WORDS)
-    end
-
-    it 'should not have any user guesses to check' do
-      expect(subject.guesses).to eq(nil)
-    end
-  end
 end
